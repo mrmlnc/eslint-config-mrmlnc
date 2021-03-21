bd81b23e64687875b9ec480788bdafa90919fff5
mrmlnc <mrmlnc@yandex-team.ru>
2021-03-21 18:34:28 +0300

    feat: update `import` version and rules

---
+++ rules/import.js
@@ -0,0 +1,66 @@
+// https://github.com/benmosher/eslint-plugin-import#static-analysis
+const IMPORT_STATIC_ANALYSIS_RULES = {
+	'import/no-unresolved': ['error', { caseSensitive: true }],
+	'import/named': 'error',
+	'import/default': 'error',
+	'import/namespace': ['error', { allowComputed: true }],
+	'import/no-restricted-paths': 'off',
+	'import/no-absolute-path': 'error',
+	'import/no-dynamic-require': 'error',
+	'import/no-internal-modules': 'off',
+	'import/no-webpack-loader-syntax': 'error',
+	'import/no-self-import': 'error',
+	'import/no-cycle': 'error',
+	'import/no-useless-path-segments': ['error', { noUselessIndex: true }],
+	'import/no-relative-parent-imports': 'off'
+};
+
+// https://github.com/benmosher/eslint-plugin-import#helpful-warnings
+const IMPORT_HELPFUL_WARNINGS_RULES = {
+	'import/export': 'error',
+	'import/no-named-as-default': 'error',
+	'import/no-named-as-default-member': 'error',
+	'import/no-deprecated': 'warn',
+	'import/no-extraneous-dependencies': 'error',
+	'import/no-mutable-exports': 'error',
+	'import/no-unused-modules': 'error'
+};
+
+// https://github.com/benmosher/eslint-plugin-import#module-systems
+const IMPORT_MODULE_SYSTEMS_RULES = {
+	'import/unambiguous': 'off',
+	'import/no-commonjs': 'error',
+	'import/no-amd': 'error',
+	'import/no-nodejs-modules': 'off'
+};
+
+// https://github.com/benmosher/eslint-plugin-import#style-guide
+const IMPORT_STYLE_GUIDE_RULES = {
+	// Produce an error for `import = require` TS syntax.
+	'import/first': 'off',
+	'import/exports-last': 'off',
+	// Produce an error when `import type` is used for default and named bindings.
+	// `import type Settings from '../settings';`
+	// `import type { Options } from '../settings';`
+	'import/no-duplicates': 'off',
+	'import/no-namespace': 'off',
+	'import/extensions': ['error', 'never', { json: 'always' }],
+	'import/order': 'error',
+	'import/newline-after-import': 'error',
+	'import/prefer-default-export': 'off',
+	'import/max-dependencies': 'off',
+	'import/no-unassigned-import': 'error',
+	'import/no-named-default': 'error',
+	'import/no-default-export': 'off',
+	'import/no-named-export': 'off',
+	'import/no-anonymous-default-export': 'error',
+	'import/group-exports': 'off',
+	'import/dynamic-import-chunkname': 'off'
+};
+
+module.exports = {
+	...IMPORT_STATIC_ANALYSIS_RULES,
+	...IMPORT_HELPFUL_WARNINGS_RULES,
+	...IMPORT_MODULE_SYSTEMS_RULES,
+	...IMPORT_STYLE_GUIDE_RULES
+};
--- rules/index.js
+++ rules/index.js
@@ -1,5 +1,6 @@
 module.exports = {
 	eslint: require('./eslint'),
 	mocha: require('./mocha'),
-	node: require('./node')
+	node: require('./node'),
+	import: require('./import')
 };
